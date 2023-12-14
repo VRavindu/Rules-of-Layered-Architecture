f2e92103ac26283bc241b64f833da2296f77f91a
VRavindu <vimukthiravindu01@gmail.com>
2023-12-14 14:33:56 +0530

    initial commit

---
+++ loose-coupling/src/main/java/lk/ijse/Boy.java
@@ -0,0 +1,8 @@
+package lk.ijse;
+
+public class Boy {
+    public void chatWithGirl(){
+        Girl girl = new Girl();
+        girl.chat();
+    }
+}
---
+++ tight-coupling/src/main/java/lk/ijse/Girl.java
@@ -0,0 +1,7 @@
+package lk.ijse;
+
+public class Girl {
+    public void chat(){
+        System.out.println("Chat....");
+    }
+}
